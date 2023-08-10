ac8924e0ee550f28564fb724cb38c87533edabef
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-10 21:32:31 +0100

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+x = number
 if number <= 0:
-    numb = -number
-    if (numb % 10) != 0:
-        print(f"Last digit of {number} is {-(numb % 10)} and is less than {6} \
-                and not {0}")
+    y = -x
+    if (y % 10) != 0:
+        print(f"Last digit of {x} is {-(y % 10)} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {numb % 10} and is {0}")
+        print(f"Last digit of {x} is {y % 10} and is {0}")
 else:
-    if (number % 10) < 6 and (number % 10) != 0:
-        print(f"Last digit of {number} is {number % 10} and is less than {6} \
-                and not {0}")
-    elif (number % 10) > 5:
-        print(f"Last digit of {number} is {number % 10} and is greater \
-                than {5}")
+    if (x % 10) < 6 and (x % 10) != 0:
+        print(f"Last digit of {x} is {x % 10} and is less than 6 and not 0")
+    elif (x % 10) > 5:
+        print(f"Last digit of {x} is {x % 10} and is greater than 5")
     else:
-        print(f"Last digit of {number} is {number % 10} and is {0}")
+        print(f"Last digit of {x} is {x % 10} and is 0")
