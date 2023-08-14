c2a0baa8c9fa59bfa7b64d9be8e98676bcbf3046
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-14 20:19:51 +0100

    lists of list = matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             if j == (len(i) - 1):
-                print("{}".format(i[j]))
+                print("{:d}".format(i[j]))
             else:
-                print("{}".format(i[j]), end=' ')
+                print("{:d}".format(i[j]), end=' ')
