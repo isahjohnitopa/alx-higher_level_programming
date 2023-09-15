399ce324e064986ee2c7d965c07d01b77bcb55f5
isahjohna <ijohnabdulmalik@gmail.com>
2023-09-15 22:53:46 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initiate a new Student
+
+        Args:
+            first_name (str): The first name of the Student.
+            last_name (str): The last name of the Student.
+            age (int): The age of the Student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of Student.
+
+        If attrs is a list of string, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (str): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attribute of the Student.
+
+        Args:
+            json (dict): The key/value pair to replace the attribute with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
